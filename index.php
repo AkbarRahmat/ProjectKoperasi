0fd5b9b417e00eab8126259bab7353e7a1a35bf5
AkbarRahmat <akbarrahmathidayatm@gmail.com>
2024-01-03 00:30:23 +0700

    update

--- index.php
+++ index.php
@@ -1,31 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style-index.css">
+  <title>Koperasi Wiatakarya Sejahtera</title>
 </head>
+
 <body>
-  <!-- navbar -->
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <p class="navbar-brand fw-bold" >Wiatakarya Sejahtera</p>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav ms-auto">
-        <a class="nav-link active" aria-current="page" href="login.php">Login</a>
-        <a class="nav-link active" aria-current="page" href="register.php">Register</a>
-      </div>
+  
+  <header>
+    Koperasi <span>Wiatakarya Sejahtera</span>
+  </header>
+  <main>
+    <div class="image"><img src="dok1.jpeg" alt="foto koperasi"></div>
+    <div class="card">
+      <h1>Selamat <br> Datang!</h1>
+      <a href="login.php" class="login">Login</a>
+      <a href="register.php" class="register">Registrasi</a>
     </div>
-  </div>
-</nav>
-<div class="badge bg-primary text-wrap" style="width: 6rem;">
-  Koperasi simpan & Pinjam
-</div>
-<img src="dok1.jpeg" class="img-fluid" alt="dokumentasi">
+  </main>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
 </html>
